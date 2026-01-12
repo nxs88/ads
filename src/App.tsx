233085a5b513b83f5a33079de362e4622d5b683e
nXs <nxs88@mail.ru>
2026-01-12 22:15:29 +0700

    SIlent refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { DashboardPage } from '@pages/dashboard/DashboardPage';
 import { LoginPage } from '@pages/login/LoginPage';
+import { refreshAccessToken } from '@shared/auth/refresh';
 import { Layout } from '@shared/ui/Layout';
 import { ProtectedRoute } from '@shared/ui/ProtectedRoute';
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+  useEffect(() => {
+    refreshAccessToken().catch(() => {});
+    // если refreshToken невалидный — просто остаёмся разлогиненными
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
